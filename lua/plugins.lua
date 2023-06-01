47e28adde559d2f241d74069fd753944ab95d3f3
Charlotte Smith <charliesmith5019@gmail.com>
2023-06-01 22:41:07 +0100

    Removed dev-icons dependency

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -135,7 +135,6 @@ local function packerStartup(use)
 
     use {
         'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true },
         config = function()
             require('lualine').setup({
                 options = {
