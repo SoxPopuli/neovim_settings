0f0e9ba33e7f20005778101d2fa60c5197aabef0
Charlotte Smith <charliesmith5019@gmail.com>
2023-07-21 14:41:47 +0100

    Added quit bind to fugitive

---
+++ after/plugin/fugitive.lua
@@ -0,0 +1,13 @@
+-- @param args table
+local function set_keybinds(args)
+   local bufnr = args.buf
+   local bind = vim.keymap.set
+
+   bind('n', 'q', '<cmd>:q<cr>', { buffer = bufnr, nowait = true })
+end
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+      group = 'fugitive',
+      pattern = 'fugitive',
+      callback = set_keybinds
+})
